728cf588317f2a98858b1655a35ddcba25c7d797
Fulano de Tal <gustavosantosteixeira98@gmail.com>
2024-05-01 05:24:41 -0300

    23545dgdfgs45269620

--- ms.js
+++ ms.js
@@ -24,17 +24,17 @@ class Matrix {
     }
     
     //method to show the matrix
-    async print() {
-        console.log('\n\nMatriz:');
+    async showMatrix() {
+        console.log('\nMatriz:');
         //loop to traverse the lines
         for (let l = 0; l < this.lines; l++) {
             console.log(this.matrix[l].join('\t'));
         }
-        console.log('\n\n');
+        console.log('\n');
     }
 
     //method to multiply two values
-    async multiplication(n1, n2) {
+    async multiplicationTwoElements(n1, n2) {
         let number = n2 * n1;
         return number;
     }
@@ -42,13 +42,12 @@ class Matrix {
 
 
     //method to divide an entire now of the matrix and simplify the values
-    async main_division(number_line, division_number_top, division_number_bottom) {
+    async fractionDivision(number_line, division_number_top, division_number_bottom) {
         //dividing the values
         for (let i = 0; i < this.lines; i++) {
             let element = this.matrix[number_line][i];
-            console.log(element);
-            element[0] = await this.multiplication(element[0], division_number_bottom);
-            element[1] = await this.multiplication(element[1], division_number_top);
+            element[0] = await this.multiplicationTwoElements(element[0], division_number_bottom);
+            element[1] = await this.multiplicationTwoElements(element[1], division_number_top);
 
             //sets the top and bottom values to 1 if they are equal
             if(element[0]===element[1]){
@@ -83,12 +82,12 @@ async function main() {
     const matrix = new Matrix(3, 3); 
     //create
     await matrix.createMatrix(elements);
-
     
-    await matrix.main_division(1, 3, 1);
+    //divide by value
+    await matrix.fractionDivision(1, 3, 1);
 
     //print
-    await matrix.print();
+    await matrix.showMatrix();
 }
 
 //calls the main method
